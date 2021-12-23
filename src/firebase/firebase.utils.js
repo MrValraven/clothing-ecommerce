788e5f46a39e39f6d58ef8d84d1a282ff346748c
mrvalraven <akatiago@gmail.com>
2021-12-23 21:07:21 +0000

    integrated firebase into application

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+const config = {
+  apiKey: "AIzaSyCPrk69ibg2709MraDY-N4MmedMl5pZTiE",
+  authDomain: "crwn-db-2a54a.firebaseapp.com",
+  projectId: "crwn-db-2a54a",
+  storageBucket: "crwn-db-2a54a.appspot.com",
+  messagingSenderId: "417564563735",
+  appId: "1:417564563735:web:b473137094bfb478887f52",
+  measurementId: "G-RKDXFZ8VRD",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
